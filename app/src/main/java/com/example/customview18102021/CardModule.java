6a274fb11e1ffa60aab378465a9abea77dba41f9
phamtanphat <phatdroid94@gmail.com>
2021-12-10 18:44:17 +0700

    attr

---
+++ app/src/main/java/com/example/customview18102021/CardModule.java
@@ -0,0 +1,22 @@
+package com.example.customview18102021;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class CardModule extends LinearLayout {
+
+    public CardModule(Context context) {
+        super(context);
+    }
+
+    public CardModule(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CardModule(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
